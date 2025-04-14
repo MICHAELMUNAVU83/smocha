4586c0c0653304eeb7a40c3902cb7e63febf296e
Mgridgelabs <mainaianndungu@gmail.com>
2025-04-14 16:24:32 +0300

    `Refactor Hero section layout in Landing component`

--- smocha/src/page/landing.jsx
+++ smocha/src/page/landing.jsx
@@ -6,15 +6,17 @@ function Landing() {
     return (
         <div>
             <Hero />
-            <div className="bg-[url('/images/bg.jpg')] bg-cover bg-center h-screen items-top justify-center"
-            >
-                <h1 className="text-5xl text-center  font-extrabold text-black ">
-                    KENYAN MENU
-                </h1>
-                <div className='mt-4'>
+            <div className="relative h-full min-h-screen">
+                {/* Background Image with opacity layer */}
+                <div className="absolute inset-0 bg-[url('/images/bg.jpg')] bg-cover bg-center opacity-90 z-0"></div>
+
+                {/* Foreground Content */}
+                <div className="relative z-10 px-4 py-10">
+                    <h1 className="text-5xl text-center font-extrabold text-black mb-6">
+                        KENYAN MENU
+                    </h1>
                     <Menu />
                 </div>
-                
             </div>
         </div>
     );
