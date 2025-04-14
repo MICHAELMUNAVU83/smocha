8e16f73c8c2472ea1e2bc49463c1329b8170c9e1
Mgridgelabs <mainaianndungu@gmail.com>
2025-04-14 20:37:11 +0300

    `Removed background image and replaced with solid background color`

--- smocha/src/page/landing.jsx
+++ smocha/src/page/landing.jsx
@@ -7,9 +7,8 @@ function Landing() {
         <div>
             <Hero />
             <div className="relative h-full min-h-screen">
-                {/* Background Image with opacity layer */}
-                {/* <div className="absolute inset-0 bg-[url('/images/bg.jpg')] bg-cover bg-center opacity-90 z-0"></div> */}
-                
+                {/* background color  */}
+                <div className="absolute top-0 left-0 w-full h-full bg-[#a13623dc]"></div>
                 {/* Foreground Content */}
                 <div className="relative z-10 px-4 py-10">
                     <h1 className="text-5xl text-center font-extrabold text-black mb-6">
